395c1e5743980613d50ac75db396b83a98fbd823
Leonel Antonio Espinoza Sotelo <leo@pop-os.localdomain>
2021-03-04 09:47:00 -0300

    fixed bugs 4

--- app/helpers/includeHtml.js
+++ app/helpers/includeHtml.js
@@ -2,6 +2,6 @@ export default function includeHtml(props){
   let { fileName, element } = props;
   fetch('app/assets/views/' + fileName)
     .then(res => res.ok ? res.text() : Promise.reject())
-    .then(async text => await element.insertAdjacentHTML('beforebegin', text))
+    .then(async text => await element.insertAdjacentHTML('beforeend', text))
     .catch(err => console.error('Error al cargar la ruta: ' + fileName + 'More Info => ' + err))
 };
